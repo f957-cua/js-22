b6dcc7329448314cc7a33f161812c35d8e453e51
ivan_filipchyk <ivanfiliptschik00@gmail.com>
2021-03-10 06:13:25 +0200

    works

--- js/04-colorpicker.js
+++ js/04-colorpicker.js
@@ -15,60 +15,156 @@ const colors = [
 ];
 
 const paletteContainer = document.querySelector('.js-palette');
-const cardsMarkup = createColorCardsMarkup(colors);
 
+//========2-вызвали функ-цию
+const cardsMarkup = createColorCardsMarkup(colors);
+//========3-повесили эту строку в выбраное место
 paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
+//=======2-ДЕЛЕГИРОВАНИЕ 
 paletteContainer.addEventListener('click', onPaletteContainerClick);
 
+//=========1-создали функцию, которая РЭНДЕРИТ разметку
+
 function createColorCardsMarkup(colors) {
-  return colors
-    .map(({ hex, rgb }) => {
-      return `
-    <div class="color-card">
-     <div><div><div> <div
-     class="color-swatch"
-     data-hex="${hex}"
-     data-rgb="${rgb}"
-     style="background-color: ${hex}"
-   ></div></div></div></div>
-      <div class="color-meta">
-        <p>HEX: ${hex}</p>
-        <p>RGB: ${rgb}</p>
-      </div>
+  return colors.map(({ hex, rgb }) => {
+    return `
+  <div class="color-card">
+    <div
+    class="color-swatch"
+    data-hex="${hex}"
+    data-rgb="${rgb}"
+    style="background-color: ${hex}"
+      ></div>
+    <div class="color-meta">
+      <p>HEX: ${hex}</p>
+      <p>RGB: ${rgb}</p>
     </div>
-    `;
-    })
-    .join('');
-}
+  </div>
+  `;
+  }).join('');
+};
+
+// шаблон одного эл-та + массив объектов
+// мэпнули + сделали карточку для каждого
+// вернули целую строку
+//------------------------------------
+
 
 function onPaletteContainerClick(evt) {
-  const isColorSwatchEl = evt.target.classList.contains('color-swatch');
-
-  if (!isColorSwatchEl) {
-    return;
+  // проверка куда мы клацнули (можно только в свотч)
+  const colorSwatchEl = evt.target.classList.contains('color-swatch');
+  if (!colorSwatchEl) {
+    return
   }
+  //---------------------------
+
+  // проверяем активные карточки
+    // ищим текущий выбранный по классу
+    // если нашли - удаляем класс
+  removeActiveCardClass()
+  //---------------------------
 
   const swatchEl = evt.target;
   const parentColorCard = swatchEl.closest('.color-card');
 
-  removeActiveCardClass();
-  addActiveCardClass(parentColorCard);
+  // затем добавляем отзывчивость карточек
+  addClassToColorCard(parentColorCard)
+ 
   setBodyBgColor(swatchEl.dataset.hex);
 }
 
 function setBodyBgColor(color) {
-  document.body.style.backgroundColor = color;
+  document.body.style.backgroundColor = color;  
 }
 
 function removeActiveCardClass() {
-  const currentActiveCard = document.querySelector('.color-card.is-active');
-
+    const currentActiveCard = document.querySelector('.color-card.is-active')
+  
   if (currentActiveCard) {
     currentActiveCard.classList.remove('is-active');
   }
 }
 
-function addActiveCardClass(card) {
+function addClassToColorCard(card) {
+    // затем + нужный class
   card.classList.add('is-active');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const paletteContainer = document.querySelector('.js-palette');
+// const cardsMarkup = createColorCardsMarkup(colors);
+
+// paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+// paletteContainer.addEventListener('click', onPaletteContainerClick);
+
+// function createColorCardsMarkup(colors) {
+//   return colors
+//     .map(({ hex, rgb }) => {
+//       return `
+//     <div class="color-card">
+//      <div><div><div> <div
+//      class="color-swatch"
+//      data-hex="${hex}"
+//      data-rgb="${rgb}"
+//      style="background-color: ${hex}"
+//    ></div></div></div></div>
+//       <div class="color-meta">
+//         <p>HEX: ${hex}</p>
+//         <p>RGB: ${rgb}</p>
+//       </div>
+//     </div>
+//     `;
+//     })
+//     .join('');
+// }
+
+// function onPaletteContainerClick(evt) {
+//   const isColorSwatchEl = evt.target.classList.contains('color-swatch');
+
+//   if (!isColorSwatchEl) {
+//     return;
+//   }
+
+//   const swatchEl = evt.target;
+//   const parentColorCard = swatchEl.closest('.color-card');
+
+//   removeActiveCardClass();
+//   addActiveCardClass(parentColorCard);
+//   setBodyBgColor(swatchEl.dataset.hex);
+// }
+
+// function setBodyBgColor(color) {
+//   document.body.style.backgroundColor = color;
+// }
+
+// function removeActiveCardClass() {
+//   const currentActiveCard = document.querySelector('.color-card.is-active');
+
+//   if (currentActiveCard) {
+//     currentActiveCard.classList.remove('is-active');
+//   }
+// }
+
+// function addActiveCardClass(card) {
+//   card.classList.add('is-active');
+// }
